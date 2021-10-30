ac65534639555f3fbf3c69079f652749ae0e83dc
rotero161 <rotero161@uoc.edu>
2021-10-30 11:50:06 +0200

    Add SHA256 example

--- hash/sha256_example.py
+++ hash/sha256_example.py
@@ -1,3 +1,13 @@
 """
 In this script we set a function for calculating the SHA256 hash for any string introduced.
 """
+
+# Import the library
+import hashlib
+
+# Encode the message to bytes
+message = "Poker de Hashes is the best group".encode()
+
+# Example with SHA-256
+
+print("SHA-256:", hashlib.sha256(message).hexdigest())
